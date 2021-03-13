f201e2f828256c81ac0e82f49cd15fdf9a1b3a40
Pierre-Louis Bertrand <pierrelouis.bertrand@gmail.com>
2021-03-13 16:51:12 +0100

    Début layout liste des pizzas d'un resto

---
+++ CroustiPizz.Mobile/CroustiPizz.Mobile/Pages/PizzaListShopPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CroustiPizz.Mobile.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CroustiPizz.Mobile.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PizzaListShopPage : ContentPage
+    {
+        public PizzaListShopPage()
+        {
+            BindingContext = new PizzaListShopViewModel();
+            InitializeComponent();
+        }
+    }
+}
---
+++ CroustiPizz.Mobile/CroustiPizz.Mobile/ViewModels/PizzaListShopViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using CroustiPizz.Mobile.Dtos;
+using CroustiPizz.Mobile.Dtos.Pizzas;
+using CroustiPizz.Mobile.Services;
+using Storm.Mvvm;
+using Xamarin.Forms;
+
+namespace CroustiPizz.Mobile.ViewModels
+{
+    public class PizzaListShopViewModel : ViewModelBase
+    {
+        private ObservableCollection<PizzaItem> _pizzas;
+
+        public ObservableCollection<PizzaItem> Pizzas
+        {
+            get => _pizzas;
+            set => SetProperty(ref _pizzas, value);
+        }
+        
+        public ICommand SelectedCommand { get; }
+
+        public PizzaListShopViewModel()
+        {
+            SelectedCommand = new Command<PizzaItem>(SelectedAction);
+        }
+
+        private void SelectedAction(PizzaItem obj)
+        {
+            
+        }
+
+        public override async Task OnResume()
+        {
+            await base.OnResume();
+
+            IPizzaApiService service = DependencyService.Get<IPizzaApiService>();
+
+            Response<List<PizzaItem>> response = await service.ListPizzas(1);
+            
+            Console.WriteLine($"Appel HTTP : {response.IsSuccess}");
+            if (response.IsSuccess)
+            {
+                Console.WriteLine($"Appel HTTP : {response.Data.Count}");
+                Pizzas = new ObservableCollection<PizzaItem>(response.Data);
+            }
+        }
+    }
+}
