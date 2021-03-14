cd777ff3a4ffc35909f9ed3541318880f47910af
Pierre-Louis Bertrand <pierrelouis.bertrand@gmail.com>
2021-03-14 19:20:23 +0100

    Tentatiive de Binding qui fait tout péter (envie de mourir)

---
+++ CroustiPizz.Mobile/CroustiPizz.Mobile/Converters/DoubleToStringConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace CroustiPizz.Mobile.Converters
+{
+    public class DoubleToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Double truc = (Double) value;
+            return truc.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- CroustiPizz.Mobile/CroustiPizz.Mobile/ViewModels/PizzaListShopViewModel.cs
+++ CroustiPizz.Mobile/CroustiPizz.Mobile/ViewModels/PizzaListShopViewModel.cs
@@ -13,14 +13,21 @@ namespace CroustiPizz.Mobile.ViewModels
 {
     public class PizzaListShopViewModel : ViewModelBase
     {
+        
         private ObservableCollection<PizzaItem> _pizzas;
-
         public ObservableCollection<PizzaItem> Pizzas
         {
             get => _pizzas;
             set => SetProperty(ref _pizzas, value);
         }
-        
+
+        private int _shopId = 1;
+        public int ShopId
+        {
+            get => _shopId;
+            set => SetProperty(ref _shopId, value);
+        }
+
         public ICommand SelectedCommand { get; }
 
         public PizzaListShopViewModel()
@@ -37,16 +44,28 @@ namespace CroustiPizz.Mobile.ViewModels
         {
             await base.OnResume();
 
-            IPizzaApiService service = DependencyService.Get<IPizzaApiService>();
-
-            Response<List<PizzaItem>> response = await service.ListPizzas(1);
-            
-            Console.WriteLine($"Appel HTTP : {response.IsSuccess}");
-            if (response.IsSuccess)
+            PizzaItem pizzaItem = new PizzaItem
             {
-                Console.WriteLine($"Appel HTTP : {response.Data.Count}");
-                Pizzas = new ObservableCollection<PizzaItem>(response.Data);
-            }
+                Id = 1,
+                Name = "truc",
+                Description = "Ntm Romain",
+                Price = 10,
+                OutOfStock = false
+            };
+            
+            _pizzas.Add(pizzaItem);
+            
+
+            // IPizzaApiService service = DependencyService.Get<IPizzaApiService>();
+            //
+            // Response<List<PizzaItem>> response = await service.ListPizzas(_shopId);
+            //
+            // Console.WriteLine($"Appel HTTP : {response.IsSuccess}");
+            // if (response.IsSuccess)
+            // {
+            //     Console.WriteLine($"Appel HTTP : {response.Data.Count}");
+            //     Pizzas = new ObservableCollection<PizzaItem>(response.Data);
+            // }
         }
     }
 }
