db5cdeac02118ffd5fd886323cd8cc978cd960f6
ionasNeonakis <ionas.neonakis@gmail.com>
2021-03-25 23:22:27 +0100

    Converter de la quantité d'éléments dans le panier

---
+++ CroustiPizz.Mobile/CroustiPizz.Mobile/Converters/QuantityConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace CroustiPizz.Mobile.Converters
+{
+    public class QuantityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            long quantite =  (long) value;
+            if (quantite >= 100)
+            {
+                return "99+";
+            }
+
+            return quantite.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
